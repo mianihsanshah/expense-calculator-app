45b0ac4a5a9ba189799bca6f7ebf853a4186bfaf
mianihsanshah <mianihsanshah@gmail.com>
2020-06-22 23:28:24 +0500

    surge testing

--- src/components.js
+++ src/components.js
@@ -47,6 +47,7 @@ function Components() {
     return (
         <div className="container">
             <h1 className="text-center">Expense Tracker</h1>
+            <h1 className="text-center">Surge Testing</h1>
 
             <h3 className="text-center">Your Balance PKR {getIncome() + getExpense()}</h3>
 
