43d4930e73be889c5e16e4a8e1546067eb62ab85
mianihsanshah <mianihsanshah@gmail.com>
2020-06-23 14:24:12 +0500

    Update components.js

--- src/components.js
+++ src/components.js
@@ -46,9 +46,7 @@ function Components() {
 
     return (
         <div className="container">
-            <h1 className="text-center">Expense Tracker</h1>
-            <h1 className="text-center">Surge Testing</h1>
-            <h1 className="text-center">Surge Testing</h1>
+            <h1 className="text-center">Expense Tracker</h1>            
 
             <h3 className="text-center">Your Balance PKR {getIncome() + getExpense()}</h3>
 
